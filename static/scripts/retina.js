20348a46384a0e2013090b58ff8553775d64e68f
howarddyer <me@howard-dyer.co.uk>
2016-04-19 22:36:43 +0100

    Add static files and references
    Move main stylesheet to static/stylesheets folder.
    Add Bootstrap recommended head content.
    Add CDN references for Bootstrap and JQuery.

---
+++ static/scripts/retina.js
@@ -0,0 +1,11 @@
+$(function () {
+  if (window.devicePixelRatio == 2) {	 
+    var images = $("img.retina");
+      for(var i = 0; i < images.length; i++) {
+      var imageType = images[i].src.substr(-4);
+      var imageName = images[i].src.substr(0, images[i].src.length - 4);
+      imageName += "@2x" + imageType;
+      images[i].src = imageName;
+    }
+  }
+});
